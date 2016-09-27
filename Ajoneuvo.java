95a8f2777063367c56a751e3cb1410357ed7d9d5
Tsereh <tserehbros@gmail.com>
2016-09-27 13:47:19 +0300

    ajoneuvon lisäys

---
+++ Ajoneuvo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekti;
+
+/**
+ *
+ * @author Kiril Tsereh
+ */
+public class Ajoneuvo {
+    private double vakuutus, vero, kulutus;
+    
+    public Ajoneuvo(double uusiVakuutus, double uusiVero, double uusiKulutus) {
+        vakuutus = uusiVakuutus;
+        vero = uusiVero;
+        kulutus = uusiKulutus;
+    }
+    
+}
