2de090d5e17999e51e78b6d1bce9411fc07b8d9a
Otto Åström <ozzy.astrom@gmail.com>
2016-10-07 11:11:04 +0300

    Update Seutu.java
    Lisätty hinnat eri seuduille.

--- src/Seutu.java
+++ src/Seutu.java
@@ -11,16 +11,99 @@
 public class Seutu {
     double hinta;
     
-    
     public double pk(String pa) {
         if (pa == "98") {
-            hinta = 1.42; // Tohon 98 pk-seutu hinta
+            hinta = 1.485; // Tohon 98 pk-seutu hinta
         }
         else if (pa == "95") {
-            hinta = 1.42; // Tohon 95 pk-seutu hinta
+            hinta = 1.408; // Tohon 95 pk-seutu hinta
         }
         else if (pa == "d") {
-            hinta = 1.42; // Tohon diesel pk-seutu hinta
+            hinta = 1.211; // Tohon diesel pk-seutu hinta
+        }
+        return hinta;
+    }
+    public double jyvaskyla(String pa) {
+        if (pa == "98") {
+            hinta = 1.481; // Tohon 98 pk-seutu hinta
+        }
+        else if (pa == "95") {
+            hinta = 1.403; // Tohon 95 pk-seutu hinta
+        }
+        else if (pa == "d") {
+            hinta = 1.213; // Tohon diesel pk-seutu hinta
+        }
+        return hinta;
+    }
+    public double tampere(String pa) {
+        if (pa == "98") {
+            hinta = 1.471; // Tohon 98 pk-seutu hinta
+        }
+        else if (pa == "95") {
+            hinta = 1.390; // Tohon 95 pk-seutu hinta
+        }
+        else if (pa == "d") {
+            hinta = 1.196; // Tohon diesel pk-seutu hinta
+        }
+        return hinta;
+    }
+    public double oulu(String pa) {
+        if (pa == "98") {
+            hinta = 1.447; // Tohon 98 pk-seutu hinta
+        }
+        else if (pa == "95") {
+            hinta = 1.363; // Tohon 95 pk-seutu hinta
+        }
+        else if (pa == "d") {
+            hinta = 1.171; // Tohon diesel pk-seutu hinta
+        }
+        return hinta;
+    }
+    public double pori(String pa) {
+        if (pa == "98") {
+            hinta = 1.407; // Tohon 98 pk-seutu hinta
+        }
+        else if (pa == "95") {
+            hinta = 1.371; // Tohon 95 pk-seutu hinta
+        }
+        else if (pa == "d") {
+            hinta = 1.186; // Tohon diesel pk-seutu hinta
+        }
+        return hinta;
+    }
+    public double turku(String pa) {
+        if (pa == "98") {
+            hinta = 1.470; // Tohon 98 pk-seutu hinta
+        }
+        else if (pa == "95") {
+            hinta = 1.386; // Tohon 95 pk-seutu hinta
+        }
+        else if (pa == "d") {
+            hinta = 1.196; // Tohon diesel pk-seutu hinta
+        }
+        return hinta;
+    }
+    public double seinajoki(String pa) {
+        if (pa == "98") {
+            hinta = 1.461; // Tohon 98 pk-seutu hinta
+        }
+        else if (pa == "95") {
+            hinta = 1.392; // Tohon 95 pk-seutu hinta
+        }
+        else if (pa == "d") {
+            hinta = 1.199; // Tohon diesel pk-seutu hinta
+        }
+        return hinta;
+    }
+    public double kokoSuomi(String pa) {
+        if (pa == "98") {
+            hinta = 1.471; // Tohon 98 pk-seutu hinta
+        }
+        else if (pa == "95") {
+            hinta = 1.393; // Tohon 95 pk-seutu hinta
+        }
+        else if (pa == "d") {
+            hinta = 1.119; // Tohon diesel pk-seutu hinta
         }
         return hinta;
     }
